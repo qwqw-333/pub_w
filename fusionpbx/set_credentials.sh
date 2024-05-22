f810ea66b75a66ced2a8eee5d35c5a760d8bbda3
qwqw-333 <132368159+qwqw-333@users.noreply.github.com>
2024-05-22 11:44:27 +0300

    write param without ""

--- fusionpbx/set_credentials.sh
+++ fusionpbx/set_credentials.sh
@@ -12,10 +12,10 @@ echo
 config="/usr/src/fusionpbx-install.sh/debian/resources/config.sh"
 
 # Change "system_password="
-sed -i "s/^system_password=.*/system_password=\"$pass\"/" "$config"
+sed -i "s/^system_password=.*/system_password=$pass/" "$config"
 
 # Change "switch_token="
-sed -i "s/^switch_token=.*/switch_token=\"$token\"/" "$config"
+sed -i "s/^switch_token=.*/switch_token=$token/" "$config"
 
 # Change "database_password="
-sed -i "s/^database_password=.*/database_password=\"$pass\"/" "$config"
+sed -i "s/^database_password=.*/database_password=$pass/" "$config"
